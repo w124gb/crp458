4ffa187fee16e528908ae2950d995b05ce266c8a
Jonathan Dixon <w124gb@users.noreply.github.com>
2018-02-18 22:33:55 -0600

    Assignment 6

---
+++ assignment6/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        alert("Text: " + $("#test").text());
+    });
+    $("#btn2").click(function(){
+        alert("HTML: " + $("#test").html());
+    });
+});
