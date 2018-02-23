354f33562a62b412192e8625d15081c75c45eef6
Jonathan Dixon <w124gb@users.noreply.github.com>
2018-02-22 19:17:17 -0600

    updated

--- assignment6/script.js
+++ assignment6/script.js
@@ -1,8 +1,33 @@
+//Use an id and button
 $(document).ready(function(){
     $("#btn1").click(function(){
-        alert("Text: " + $("#test").text());
+        $("#ex1").hide();
     });
     $("#btn2").click(function(){
-        alert("HTML: " + $("#test").html());
+        $("#ex1").show();
+    });
+});
+
+	//span
+	$(".green").click(function(){
+        $("#colorChange").css('color','green');
+    });
+  
+  $(".red").click(function(){
+        $("#colorChange").css('color','red');
+    });
+	
+	$(".blue").click(function(){
+        $("#colorChange").css('color','blue');
+    });
+	
+	$(".black").click(function(){
+        $("#colorChange").css('color','black');
+    });
+
+  //Slide
+  $(document).ready(function(){
+    $("#slide").click(function(){
+        $("#slide2").slideToggle("fast");
     });
 });
